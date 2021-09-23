cad25217ddfa29491326067fc241574846f97907
Davronbek Boltayev <74819987+firdavsDev@users.noreply.github.com>
2021-09-23 16:35:30 +0500

    travel

---
+++ accounts/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, "login.html")
+
+def register(request):
+
+    if request.method == 'POST':
+
+        name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username tanlangan')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                 messages.info(request,'Email tanlangan')
+                 return redirect('register')
+            else:
+                user=User.objects.create_user(first_name=name, last_name=last_name, username=username,password=password1,email=email)
+                user.save();
+                messages.info(request,'Ruyhatdan utdingiz Tabriklayman!!!')
+                return redirect('login')
+        
+        else:
+            messages.info(request,'Not matching...')
+            return redirect('register')
+        return redirect('/')
+    else:
+        return render(request,'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
